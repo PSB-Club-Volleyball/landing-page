5785641f23f27f7104a4e76b6c326ccf2ff6a58a
Ethan Luh <ethanluh@gmail.com>
2026-08-01 07:13:16 -0400

    Link Erie Hall location to a Google Maps address (#13)
    Erie Hall now links to a map for 4711 College Dr, Erie, PA 16510.

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,12 @@ export const INSTAGRAM_URL = 'https://www.instagram.com/psbclubvolleyball'
 
 export const GROUPME_URL = 'https://groupme.com/join_group/109663906/1gjcnSbW'
 
+export const LOCATION_NAME = 'Erie Hall'
+export const LOCATION_ADDRESS = '4711 College Dr, Erie, PA 16510'
+export const LOCATION_MAP_URL = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+  `${LOCATION_NAME}, ${LOCATION_ADDRESS}`
+)}`
+
 export const BOARD = [
   { role: 'President', name: 'Preston Lam' },
   { role: 'Vice President', name: 'Lucas Mosher' },
